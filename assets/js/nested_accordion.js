24cae46287db270da672916e0413d52508874988
Jordan Long <jordan.long222@gmail.com>
2016-05-30 21:25:25 -0400

    Add JS attempts

--- assets/js/nested_accordion.js
+++ assets/js/nested_accordion.js
@@ -1,8 +1,38 @@
-//array for event listner? when _ index is clicked, change class to display
 
-var mainList = document.getElementsByClassName('hide_list');
 
-for (i=0; i=mainList.length; i++)
-  mainList[i].addEventLister("click", function fshow() {
-    mainList.setAttribute("class", "show_list");
-  });
+
+
+
+
+//ANOTHER FAILED ATTEMPT
+// function changeClassName (){                               creating a function to change class
+//   var headerClick = document.getElementsById("header1");   added ID to HTML in hopes to grab the ID and change class
+//   var currentClass = headerClick.className;                create variable currentclass to check which class is currently on element
+//   if (currentClass == "hide_list") {                       if class is hide_list, change class to show_list
+//     headerClick.className = "show_list";
+//   } else {                                                 otherwise, make it hide_list
+//     headerClick.className = "hide_list";
+//   }
+// }
+
+
+// ATTEMPT
+// var headerList = document.getElementsByClassName('hide_list');
+// for (var liHead = 0; liHead < listarray.length; liHead++) {
+//       headerList[liHead].addEventListener('click', function()
+//
+//     )
+//       setAttribute('id', 'show_list');
+// }
+
+
+
+// ATTEMPT
+// for (i = 0; i < classChange.length; i++){              loop through to see class change
+//   classChange[0].setAttribute('class', 'show_list')
+// //onclick var to index number?
+// }
+// var classChange = document.getElementsByClassName('hide_list'); grab elements by class name "hide_list"
+// if (classChange.addEventListener) {
+//   classChange('click', fshow())
+// }
