4c261a60b8d38525ef8a7c99a3c1b38799204293
Jordan Long <jordan.long222@gmail.com>
2016-06-01 14:17:47 -0400

    Working JS

--- assets/js/nested_accordion.js
+++ assets/js/nested_accordion.js
@@ -2,12 +2,18 @@ $(function (){
 	  'use strict';
 });
 
-$('.headLi').click(function () {
-  $(this).next('div').removeClass('hide_list').addClass('show_list');
-    $('.headLi').click(function(){
-      $(this).next('div').removeClass('show_list').addClass('hide_list');
-  });
+//When the header is clikced, toggle the next div to .show_list
+$('.headLi').on('click', function () {
+  $(this).next('div').slideToggle('.show_list');
 });
+
+//Only allows 2 clicks before the function doesn't work anymore
+// $('.headLi').click(function () {
+//   $(this).next('div').removeClass('hide_list').addClass('show_list');
+//     $('.headLi').click(function(){
+//       $(this).next('div').removeClass('show_list').addClass('hide_list');
+//   });
+// });
 
 
 //TEST CONNECTION///////
@@ -20,3 +26,12 @@ $('.headLi').click(function () {
 // $('.rightArrowblock').click(function () {
 //   $('div').next('p').toggleSlide(this);
 // })
+
+
+/////////////////////////////////
+/////////////////////////////////
+/////////////////////////////////
+//TOGGLE SUBLIST TO SHOW/HIDE CSS WHEN CLICKED//
+$('.r_smallArrowblock').on('click', function () {
+  $(this).next('div').slideToggle('.sub_show');
+});
